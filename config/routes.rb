8ee422a10eeccc0699721991eeabb1844c7ffff1
Álvaro Fernando <alvarofernandoms@gmail.com>
2016-04-15 01:00:52 -0300

    Changing index page with the visualizations examples

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'static#index'
-  get '/', :to => redirect('/radar_chart')
+  get '/', :to => redirect('/index')
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
