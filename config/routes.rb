42abaac85d01ba632d5189a7a943233e49f3a7ef
Álvaro Fernando <alvarofernandoms@gmail.com>
2016-04-15 00:55:22 -0300

    Rename radar chart page

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'static#index' 
-  get '/', :to => redirect('/myown')
+  # root 'static#index'
+  get '/', :to => redirect('/radar_chart')
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
