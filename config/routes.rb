1dad2892a548b333f9df29073076baea2d96f44a
Álvaro Fernando <alvarofernandoms@gmail.com>
2016-04-21 16:29:48 -0300

    :feet: Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'static#index'
   get '/', :to => redirect('/index')
+  get 'radar_chart', :to => redirect('/radar_chart')
+  get 'node_link_tree', :to => redirect('/node_link_tree')
+  get 'node_link_with_interation', :to => redirect('/node_link_with_interation')
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
